a1770dcc862abbe1c1950a658875a4627a5d03df
许奇10066485 <xu.qi2@zte.com.cn>
2019-05-27 20:12:59 +0800

    update Args schema

--- Args/src/main/scala/io/neal/Args.scala
+++ Args/src/main/scala/io/neal/Args.scala
@@ -3,9 +3,12 @@ package io.neal
 class Args(schema: String, args: Array[String]) {
 
   private lazy val internalSchema = {
-    val item = schema.split(",").map(_.trim).filter(_.nonEmpty)
+    val items = schema.split(",").map(_.trim).filter(_.nonEmpty)
     val pattern = "([a-zA-Z])(.*)".r
-
+    items.map{i =>
+      val pattern(key, value) = i
+      (key, value)
+    }.toMap
   }
 
   def this(schema: String) = {
