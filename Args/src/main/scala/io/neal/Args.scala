5115d93d7902d1ddc06a659b3530afd06df3dab2
许奇10066485 <xu.qi2@zte.com.cn>
2019-05-28 08:37:41 +0800

    parse and query schema

--- Args/src/main/scala/io/neal/Args.scala
+++ Args/src/main/scala/io/neal/Args.scala
@@ -1,18 +1,20 @@
 package io.neal
 
-class Args(schema: String, args: Array[String]) {
+class Args(schema: String, args: Array[String] = Array.empty) {
 
   private lazy val internalSchema = {
     val items = schema.split(",").map(_.trim).filter(_.nonEmpty)
     val pattern = "([a-zA-Z])(.*)".r
-    items.map{i =>
-      val pattern(key, value) = i
-      (key, value)
-    }.toMap
+    items.map {
+      case pattern(k, v) => (k, v)
+      case s: String =>
+        println(s"invalid flag: $s")
+        ("", "")
+    }.filter(_._1.nonEmpty).toMap
   }
 
-  def this(schema: String) = {
-    this(schema, Array.empty)
+  def querySchemaBy(key: String): Option[String] = {
+    internalSchema.get(key)
   }
 
 }
--- Args/src/test/scala/io/neal/ArgsTest.scala
+++ Args/src/test/scala/io/neal/ArgsTest.scala
@@ -5,12 +5,27 @@ import org.scalatest.BeforeAndAfterAll
 
 class ArgsTest extends FlatSpec with BeforeAndAfterAll {
 
-  private val schema = "l,p#,d*"
+  private val flags = List(("l", ""), ("p", "#"), ("d", "*"))
+  private val schema = flags.map(x => x._1 + x._2).mkString(",")
 
-  private var instance: Args = _
+  private var args: Args = _
 
   override def beforeAll(): Unit = {
-    instance = new Args(schema)
+    args = new Args(schema)
+  }
+
+  override def afterAll(): Unit = {
+    args = _
+  }
+
+  "Specify valid flag key" should "get flay type" in {
+    assertResult(Some(""))(args.querySchemaBy("l"))
+    assertResult(Some("#"))(args.querySchemaBy("p"))
+    assertResult(Some("*"))(args.querySchemaBy("d"))
+  }
+
+  "Specify invalid flag key" should "get none" in {
+    assertResult(None)(args.querySchemaBy("a"))
   }
 
 
